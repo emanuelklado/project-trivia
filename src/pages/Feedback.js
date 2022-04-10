c0d4b03f53cd0c43acbcbf9dcc63b667e0f89f1f
emanuelklado <emanuelklado@msn.com>
2022-04-10 00:25:35 -0300

    requisito 15 ok

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 export default class Feedback extends Component {
+  redirectToHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div data-testid="feedback-text">
         <Header />
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          onClick={ this.redirectToHome }
+        >
+          Play Again
+
+        </button>
       </div>
 
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropTypes.shape.isRequired,
+};
